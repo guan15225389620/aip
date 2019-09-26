618280886939f6ef814ff261be5f03357f3d7a58
管新光 <guanxinguang@apkpure.net>
2019-09-26 16:18:11 +0800

    update

--- library/models/chat.js
+++ library/models/chat.js
@@ -1,8 +1,8 @@
 module.exports = function (sequelize, DataTypes) {
     var m = sequelize.define('chat', {
         id: {type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true},
-        chatid:DataTypes.INTEGER,
-        openid: DataTypes.STRING,
+        chatid:DataTypes.STRING,
+        openid: DataTypes.INTEGER,
         loginid: DataTypes.STRING,
         ocr: {type: DataTypes.JSON}
     }, {
