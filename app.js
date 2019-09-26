ef9e76b8c54d00c1e4a6ffb8be2e12111e2eabee
管新光 <guanxinguang@apkpure.net>
2019-09-26 15:50:27 +0800

    update

--- app.js
+++ app.js
@@ -115,7 +115,7 @@ app.post("/login", (req, res) => {
 
 app.post("/getChatId", (req, res) => {
     var loginid = req.body.code;
-    var chatid = GenNonDuplicateID(2)；
+    var chatid = GenNonDuplicateID(2);
 
     var model = {
         chatid: chatid,
